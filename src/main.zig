cc12d3b68c51a5a6fa7f523ebcb8117c26dd27ea
freegamerskids <freegamerskids@gmail.com>
2025-11-21 20:16:04 +0100

    :p

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,12 @@
+const std = @import("std");
 const granite = @import("granite");
 
 pub fn main() !void {
-    var app = granite.App.init(800, 450, "folly", "editor");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var app = granite.App.init(allocator, 800, 450, "folly", "editor");
 
     try app.run();
 }
